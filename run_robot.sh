bacc631155540333008078ac6bd84516a66f7928
yazid9871 <yazid9871@users.noreply.github.com>
2026-09-03 21:16:58 +0100

    Generate merged Allure test reports

--- run_robot.sh
+++ run_robot.sh
@@ -3,4 +3,7 @@ set -e
 
 test_path="${MODULE_PATH:-/tests/TestSuite/ADMIN_ACCOUNT/Achats}"
 xvfb-run -a --server-args="-screen 0 1920x1080x24" \
-  robot -d /tests/results "$test_path"
+  robot \
+  --listener allure_robotframework:/tests/results/allure-results \
+  -d /tests/results \
+  "$test_path"
