5d5152f9bb81e6a3f4e7a353bff885484eaa778e
yazid9871 <yazid9871@users.noreply.github.com>
2026-09-10 18:44:31 +0100

    Make robot workflow manual and dynamic

--- run_robot.sh
+++ run_robot.sh
@@ -5,6 +5,15 @@ test_path="${MODULE_PATH:-/tests/TestSuite/ADMIN_ACCOUNT/Achats}"
 account_name="$(basename "$(dirname "$test_path")")"
 module_name="$(basename "$test_path")"
 
+if [ "$account_name" = "TestSuite" ]; then
+  account_name="$module_name"
+  module_name=""
+fi
+
+if [ "$account_name" = "FREEMIUM_ACCOUNT" ]; then
+  account_name="FREEMIUM"
+fi
+
 set +e
 xvfb-run -a --server-args="-screen 0 1920x1080x24" \
   robot \
